229afb0efc1fa821b44d05ad773d3f8c131b7a83
Ubuntu <azureuser@vm123.rt0gqnvhiahebd4etqvdfhoy0c.bx.internal.cloudapp.net>
2016-09-13 21:56:56 +0000

    add server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+// http://stackoverflow.com/questions/6084360/using-node-js-as-a-simple-web-server
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8080, function(){
+    console.log('Server running on 8080...');
+});
